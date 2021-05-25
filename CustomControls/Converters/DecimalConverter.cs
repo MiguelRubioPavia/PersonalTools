69ee1b1f31ef725b9b575ef49809f28f27f59197
Hui Yang <maromanospain@hotmail.es>
2021-05-25 18:48:25 +0200

    Cambio nombre proyecto y añadido un par de clases

---
+++ CustomControls/Converters/DecimalConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfLibrary.Converters
+{
+    [ValueConversion(typeof(decimal?), typeof(string))]
+    public class DecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((decimal?)value).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (decimal.TryParse(value as string, out decimal result))
+                return result;
+
+            return null;
+        }
+    }
+}
